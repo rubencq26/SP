c656e13adc046b390ee3b8fc971fd80bce957a8b
Ruben Conde <144829066+rubencq26@users.noreply.github.com>
2026-04-29 12:55:50 +0200

    Enhance centroid visualization and add video output
    Updated the application to visualize centroids of detected groups with a minimum pixel threshold. Added video output functionality and improved centroid detection.

--- MATLAB/SeguimientoObjetos/04_Aplicacion_Clasificador/aplicacionClasificadorImagen.m
+++ MATLAB/SeguimientoObjetos/04_Aplicacion_Clasificador/aplicacionClasificadorImagen.m
@@ -11,48 +11,263 @@ load("03_Diseño_Clasificador_Esferas\umbral_conectividad.mat");
 
 addpath("Funciones\");
 
+video = VideoWriter("Videos\video1.avi");
+video.FrameRate = 7;
+video.Quality = 100;
+
 siz = size(FramesVideo);
 
 XCol = X(Y==1,:);
 XFondo = X(Y==0,:);
-esferas = funcion_kmeans(XCol, XFondo, 8);
+esferas = funcion_kmeans(XCol, XFondo, 20);
+
+open(video);
+for i = 1 : siz(4)
+    I = FramesVideo(:,:,:,i);
+    
+    Mascara2D = calcula_deteccion_multiples_esferas(I, esferas);
+    
+   
+    Ietiq = bwareaopen(Mascara2D, round(NumPix), 8);
+    [Ietiq, N] = bwlabel(Ietiq, 8);
+    
+
+    Centroides = regionprops(Ietiq, "Centroid");
+    
+    for j = 1 : N
+        Cx = round(Centroides(1).Centroid(1));
+        Cy = round(Centroides(1).Centroid(2));
+        
+     
+        rangoY = max(1, Cy-3) : min(siz(1), Cy+3);
+        rangoX = max(1, Cx-3) : min(siz(2), Cx+3);
+        I(rangoY, rangoX, :) = 255;
+     end
+    
+    
+    imshow(I);
+    writeVideo(video, I);
+    drawnow;
+end
+close(video);
+
+implay("Videos/video1.avi");
+
+%4.2.- Visualizar sobre el frame original todos los píxeles y el centroide de la agrupación con
+%mayor número de píxeles. Para visualizar los píxeles se debe utilizar un color que los distinga.
+% El centroide debe visualizarse mediante una caja blanca 7x7.
+
+clc, clear, close all;
+
+load("01_Generacion_del_material\FramesVideo.mat");
+load("02_Extraer_RepresentarDatos\conjunto_modificado.mat");
+load("03_Diseño_Clasificador_Esferas\centroradio.mat");
+load("03_Diseño_Clasificador_Esferas\umbral_conectividad.mat");
+
+addpath("Funciones\");
+
+video = VideoWriter("Videos\video2.avi");
+video.FrameRate = 7;
+video.Quality = 100;
+
+siz = size(FramesVideo);
+
+XCol = X(Y==1,:);
+XFondo = X(Y==0,:);
+esferas = funcion_kmeans(XCol, XFondo, 20);
+
+open(video)
 
 for i = 1 : siz(4)
     I = FramesVideo(:,:,:,i);
     
     Mascara2D = calcula_deteccion_multiples_esferas(I, esferas);
     
-    % Filtrar por conectividad
+    
     Ietiq = bwlabel(Mascara2D, 8);
     Ietiq = bwareaopen(Ietiq, round(NumPix), 8);
 
     stats = regionprops(Ietiq, 'Area');
     if ~isempty(stats)
-        % 1. Ordenamos las áreas de mayor a menor
-        % Ord(1) nos da el número de etiqueta del objeto con más área
+        
         [~, Ord] = sort([stats.Area], 'descend');
     
-        % 2. Filtramos la imagen de etiquetas para quedarnos SOLO con el más grande
-        % Buscamos los píxeles cuyo valor de etiqueta sea Ord(1)
+       
         Iet = (Ietiq == Ord(1)); 
     
-        % 3. Ahora sacamos el centroide de esta nueva máscara limpia
+        
         Centroides = regionprops(Iet, "Centroid");
     
         if ~isempty(Centroides)
             Cx = round(Centroides(1).Centroid(1));
             Cy = round(Centroides(1).Centroid(2));
         
-            % Dibujar caja 7x7
             rangoY = max(1, Cy-3) : min(siz(1), Cy+3);
             rangoX = max(1, Cx-3) : min(siz(2), Cx+3);
-            I(rangoY, rangoX, 1) = 255;
-            I(rangoY, rangoX, 2:3) = 0;
+            I(rangoY, rangoX, :) = 255;
+            
         end
     end
     
     imshow(I);
     drawnow;
+    writeVideo(video,I);
 end
 
+close(video);
 
+implay("Videos/video2.avi");
+
+
+% 4.3.- Visualizar sobre el frame original las siguientes agrupaciones y centroides:
+% − En cian: los píxeles de todas las agrupaciones detectadas. No hay que visualizar ningún centroide.
+% − En azul, la agrupación mayor de píxeles y su centroide mediante una caja blanca 7x7.
+% − En verde: la segunda agrupación mayor de píxeles y su centroide mediante una caja blanca 7x7.
+clc, clear, close all;
+
+load("01_Generacion_del_material\FramesVideo.mat");
+load("02_Extraer_RepresentarDatos\conjunto_modificado.mat");
+load("03_Diseño_Clasificador_Esferas\centroradio.mat");
+load("03_Diseño_Clasificador_Esferas\umbral_conectividad.mat");
+
+addpath("Funciones\");
+video = VideoWriter("Videos\video3.avi");
+video.FrameRate = 7;
+video.Quality = 100;
+
+siz = size(FramesVideo);
+
+XCol = X(Y==1,:);
+XFondo = X(Y==0,:);
+esferas = funcion_kmeans(XCol, XFondo, 20);
+
+open(video);
+for i = 1 : siz(4)
+    I = FramesVideo(:,:,:,i);
+    %cian
+    Mascara2D = calcula_deteccion_multiples_esferas(I, esferas);
+    Ib = logical(Mascara2D);
+    
+    R = I(:,:,1);
+    G = I(:,:,2);
+    B = I(:,:,3);
+
+    R(Ib) = 0;
+    G(Ib) = 255;
+    B(Ib) = 255;
+    
+    I = cat(3,R,G,B);
+    % azul
+    Ietiq = bwlabel(Ib, 8);
+    stats = regionprops(Ietiq, 'Area');
+      if ~isempty(stats)
+        
+        [~, Ord] = sort([stats.Area], 'descend');
+        Iet = (Ietiq == Ord(1)); 
+        if length(Ord) > 1
+            Iet2 = (Ietiq == Ord(2)); 
+        end
+        Centroides = regionprops(Iet, "Centroid");
+        
+        R = I(:,:,1);
+        G = I(:,:,2);
+        B = I(:,:,3);
+
+        R(Iet) = 0;
+        G(Iet) = 0;
+        B(Iet) = 255;
+        if length(Ord) > 1
+            R(Iet2) = 0;
+            G(Iet2) = 255;
+            B(Iet2) = 0;
+        end
+        
+
+    
+        I = cat(3,R,G,B);
+
+        if ~isempty(Centroides)
+            Cx = round(Centroides(1).Centroid(1));
+            Cy = round(Centroides(1).Centroid(2));
+        
+            rangoY = max(1, Cy-3) : min(siz(1), Cy+3);
+            rangoX = max(1, Cx-3) : min(siz(2), Cx+3);
+            I(rangoY, rangoX, :) = 255;
+            
+        end
+      end
+
+    
+    imshow(I);
+    drawnow;
+    writeVideo(video, I);
+end
+
+close(video);
+
+implay("Videos/video3.avi");
+
+
+
+% 4.4.- Visualizar sobre el frame original los píxeles y el centroide de la agrupación mayor de
+% píxeles en movimiento para los siguientes casos: 
+
+% a) Consideraremos que una agrupación de píxeles detectados del color de seguimiento está
+% en movimiento si al menos uno de sus píxeles presenta variaciones de intensidad
+% significativas (diferencia de intensidad en valor absoluto mayor que 50) respecto al
+% penúltimo frame capturado.
+
+clc, clear, close all;
+
+load("01_Generacion_del_material\FramesVideo.mat");
+load("02_Extraer_RepresentarDatos\conjunto_modificado.mat");
+load("03_Diseño_Clasificador_Esferas\centroradio.mat");
+load("03_Diseño_Clasificador_Esferas\umbral_conectividad.mat");
+
+addpath("Funciones\");
+video = VideoWriter("Videos\video4a.avi");
+video.FrameRate = 7;
+video.Quality = 100;
+
+siz = size(FramesVideo);
+
+XCol = X(Y==1,:);
+XFondo = X(Y==0,:);
+esferas = funcion_kmeans(XCol, XFondo, 20);
+
+open(video);
+
+I = FramesVideo(:,:,:,1);
+
+for i = 1 : siz(4)
+    I2 = rgb2gray(I);
+    I = FramesVideo(:,:,:,i);
+    
+    Mascara2D = calcula_deteccion_multiples_esferas(I, esferas);
+
+    Ietiq = bwlabel(Mascara2D, 8);
+    Ietiq = bwareaopen(Ietiq, round(NumPix), 8);
+
+    stats = regionprops(Ietiq, 'Area');
+    Iet = zeros(siz(1), siz(2), 'logical');
+    if ~isempty(stats)
+        [~, Ord] = sort([stats.Area], 'descend');
+        Iet = (Ietiq == Ord(1)); 
+    end
+    In = rgb2gray(I);
+    Iabs = imabsdiff(In,I2);
+    
+    Ifin = Iabs(Iet);
+
+    if max(Ifin(:)) < 50
+        Iet= zeros(siz(1), siz(2), 'logical');
+
+    end
+    Ivisualiza = zeros(siz(1), siz(2), 'uint8');
+    Ivisualiza(Iet) = Iabs(Iet);
+    imshow(Ivisualiza);
+    drawnow;
+    writeVideo(video, Ivisualiza);
+
+end
+close(video);
